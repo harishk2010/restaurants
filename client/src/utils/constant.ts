d44e5d953b32a6c1dc7c7d0bd8c3976564f03b1d
harishk2010 <harish200126@gmail.com>
2025-04-09 19:04:34 +0530

    changed env in client side

--- client/src/utils/constant.ts
+++ client/src/utils/constant.ts
@@ -1,4 +1,4 @@
 // export const USER_SERVICE_URL = import.meta.env.VITE_USER_SERVICE_URL || 'http://localhost:5173';
-// export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5005';
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5005';
 export const USER_SERVICE_URL = 'user-service';
-export const BACKEND_URL = 'http://localhost:5005';
+// export const BACKEND_URL = "https://restaurants-wayc.onrender.com" ||'http://localhost:5005';
